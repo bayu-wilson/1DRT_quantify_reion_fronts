bb719b362fe2ae0ac3bf6e0d9d4535e458be7ced
Bayu Wilson <bayuwilson@ucr-secure-26-10-13-152-177.wnet.ucr.edu>
2022-05-18 20:15:06 -0700

    Adding Chris' subroutines

--- data_funcs.h
+++ data_funcs.h
@@ -8,4 +8,6 @@ double calc_mass_avg(double f[], double rho[], double m, int n);
 //Calculate ionization front position and velocity
 double* calc_ifront(double x[], double xprev[], double radius[], int n);
 double* calc_ifront_flux_method();
+double* tau_bf(double wav_em[], double NHI[]);
+void calc_mock_QSO_spec();
 #endif
