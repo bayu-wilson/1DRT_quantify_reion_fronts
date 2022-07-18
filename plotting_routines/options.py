e47e17b169444858b9592f5ff112fd93ddd0b5f1
Bayu Wilson <bayuwilson@Bayus-Air.lan>
2022-07-17 18:07:37 -0700

    deleting unneeded files

--- plotting_routines/options.py
+++
@@ -1,4 +0,0 @@
-# home_dir = "/Volumes/Extreme SSD/anson/benchmark_optimize_1d_rt_code/"
-home_dir = "/Volumes/Extreme SSD/anson/master_1d_rt/"
-figure_dir = home_dir+"plotting_routines/figures/"
-output_dir = home_dir+"output_files/"
