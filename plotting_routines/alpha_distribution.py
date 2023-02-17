971a937a986ae1fb290d1f2dd71f1c6bec44967d
Bayu Jarod Wilson <bwils033@ucr.edu>
2023-02-16 17:53:55 -0800

    update of alpha code

---
+++ plotting_routines/alpha_distribution.py
@@ -0,0 +1,51 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#initial figure parameters
+fontsize=14
+tick_dir='in'
+lw=1.5
+major_tick_size = 5
+minor_tick_size = 3
+colors = ['red', 'orange','gold','green','blue','purple','cyan','pink']
+plt.rcParams['font.family'] = "serif"
+plt.rcParams['lines.linewidth'] = lw
+plt.rcParams['font.size'] = fontsize
+plt.rcParams['xtick.direction'] = tick_dir
+plt.rcParams['ytick.direction'] = tick_dir
+plt.rcParams['xtick.major.size'] = major_tick_size
+plt.rcParams['xtick.minor.size'] = minor_tick_size
+plt.rcParams['ytick.major.size'] = major_tick_size
+plt.rcParams['ytick.minor.size'] = minor_tick_size
+
+#Load alpha matrix 
+X = np.loadtxt("../results/matrices_alpha/alpha_matrix.txt")
+
+#Define x-axis data (distance traveled through ionized IGM) 
+los_pos_bin_edges = np.linspace(0,2500,11)/1e3 #pMpc
+delta_r = np.median(los_pos_bin_edges[1:]-los_pos_bin_edges[:-1])
+los_pos_bin_centers = los_pos_bin_edges[:-1]+delta_r/2
+
+#Define what skewers to cycle through
+skewer_array = np.arange(0,30,1)
+skewer_array = np.delete(skewer_array,[0,2,3,4,16,18,20,21,22,23,27,28]) #these have high density spikes that produce outliers
+
+#Plotting each skewer's alpha distribution
+fig,ax = plt.subplots(1,figsize=(6,5))
+for i in skewer_array:
+    ax.plot(los_pos_bin_centers,X[i],alpha=0.75,color='gray')
+
+mean_per_bin = np.nanmean(X[skewer_array],axis=0)
+std_per_bin = np.nanstd(X[skewer_array],axis=0)
+ax.plot(los_pos_bin_centers,mean_per_bin,lw=5,color='red')
+ax.fill_between(los_pos_bin_centers, y1=mean_per_bin-std_per_bin, y2=mean_per_bin+std_per_bin,
+                 alpha=0.4,color='red')
+
+ax.set_ylabel(r"incident spectral index, $\alpha$")
+ax.set_xlabel("Distance traveled through \ninhomogeneous, ionized medium [pMpc]")
+ax.set_xlim(0,2.25)
+ax.set_ylim(0.95,1.5)
+
+plt.savefig("figures/alpha_distribution.pdf",bbox_inches='tight')
+plt.close()
+
---
+++ results/calculate_alpha_matrix.py
@@ -0,0 +1,66 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import glob
+import pandas as pd
+import os
+from scipy.optimize import fsolve
+
+#constants
+h_eV = 4.135667e-15
+nu_HI = 13.6/h_eV
+beta=2.75
+m_H = 1.672622e-24 #g
+Y=0.24
+
+#functions
+def phi_ndot(nu_i):
+    return alpha/nu_HI/(1-4**(-alpha))*(nu_i/nu_HI)**(-alpha-1)
+def sigmaHI_over_sigmaHI0_numeric(nu):
+    return (nu/nu_HI)**(-beta)
+def sigma_over_sigmaHI0_analytic(alpha):
+    num = alpha * (1 - 4**(-alpha-beta))
+    den = (alpha + beta)*(1 - 4**(-alpha))
+    return num/den
+def find_root_sigma(alpha,*C):
+    return sigma_over_sigmaHI0_analytic(alpha)-C
+
+
+column_names = ["los pos[pkpc]","rho","ne","dene_dt","n_tot","T[K]","xHI","xHeI","xHeII","gamma_HI","q_lya"]
+los_pos_bins = np.linspace(0,2500,11)
+alpha_matrix = np.zeros((30+1,10))
+for i in range(0,30+1):
+    skewer_number = "{:04d}".format(i)
+    dir_path = "../output_files/gasprops/sk{}_hardRun/".format(skewer_number)
+
+    print(dir_path)
+
+    n=1
+    gasprop_path = dir_path+"n{}_gasprops.txt".format(n)
+    spectrum_path = dir_path+"n{}_spectrum.txt".format(n)
+    alpha_list_i=[]
+    length_list_i=[]
+    while os.path.exists(gasprop_path):
+        df_gasprops = pd.read_csv(gasprop_path,delim_whitespace=True,skiprows=1,names=column_names)
+        los_pos = df_gasprops["los pos[pkpc]"]-df_gasprops["los pos[pkpc]"].iloc[0]
+
+        nu,I_nu = np.loadtxt(spectrum_path).T
+        sigma_nu = sigmaHI_over_sigmaHI0_numeric(nu)
+        avg_sigma_log = np.trapz(sigma_nu*I_nu/nu,x=nu)/np.trapz(I_nu/nu,x=nu)
+        my_alpha = fsolve(func=find_root_sigma, x0=1.0,args=avg_sigma_log)[0]
+        alpha_list_i.append(my_alpha)
+        length_list_i.append(np.interp(0.01,df_gasprops["xHI"],los_pos))
+        n+=1
+        gasprop_path = dir_path+"n{}_gasprops.txt".format(n)
+        spectrum_path = dir_path+"n{}_spectrum.txt".format(n)
+    cut_bins = pd.cut(length_list_i,bins=los_pos_bins)
+    series = pd.DataFrame({"los pos[pkpc]":length_list_i,"alpha":alpha_list_i})["alpha"] 
+    alpha_matrix[i] = series.groupby(cut_bins).median().values
+    #alpha_mean_i = series.groupby(cut_bins).median().values()
+    #print(alpha_mean_i)
+    #alpha_mean_i = pd.DataFrame({"los pos[pkpc]":length_list_i,"alpha":alpha_list_i})["alpha"].groupby(cut_bins).mean().values()
+    #alpha_matrix[i] = alpha_mean_i
+    #print(alpha_mean_i)
+#print(alpha_matrix)
+
+np.savetxt(fname="matrices_alpha/alpha_matrix.txt",X = alpha_matrix)
+
